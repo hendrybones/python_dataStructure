75fc02aa1a81dbb4dc9ab19f807e72cb326a78f5
Mr bones <hendrymwamburi56@gmail.com>
2023-08-22 06:11:36 +0300

    adding initial commit

--- hacker_rank_solutions.py
+++ hacker_rank_solutions.py
@@ -29,6 +29,26 @@ def count_strings(string, substring):
     return total
 
 
+a = "abcdefghijklmnopqrstuvwxyz"
+
+
+def print_rangoli(size):
+    line = []
+    for row in range(size):
+        print_ = "-".join(a[row:size])
+        line.append(print_[::-1] + print_[1:])
+    width = len(line[0])
+    for row in range(size - 1, 0, -1):
+        print(line[row].center(width, '-'))
+
+    for row in range(size):
+        print(line[row].center(width, '-'))
+
+def solve(s):
+    ans = s.split(' ')
+    ans1 = (((i.capitalize() for i in ans)))
+    return ' '.join(ans1)
+
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
